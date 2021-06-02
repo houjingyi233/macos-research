427710d0906cda6b79d295ca62b506520a6af805
Hoyt LLC Research <xss@xss.cx>
2021-06-02 09:24:23 -0400

    Updated Comments
    Updated Comments

--- sprr/sprr-commented.c
+++ sprr/sprr-commented.c
@@ -102,12 +102,9 @@ SYS_SPRR_PERM_EL1 sys_reg(3, 6, 15, 1, 6)
 #include <sys/utsname.h>
 #include <ucontext.h>
 
-/* Recover from Protected Page Access, setup Signal Handler, set x0 to a 0xdeadbeef (0x41414141) set pc +4, jmp to link register (__ss.__lr) to Return. TODO, Fuzz the pc 
-       See URL https://github.com/apple/darwin-xnu/blob/main/bsd/sys/_types/_ucontext.h for struct def
-*/
-       
 static void sev_handler(int signo, siginfo_t *info, void *cx_)
 {
+/*    printf("Now in sev_handler)\n"); */
     (void)signo;
     (void)info;
     ucontext_t *cx = cx_;
@@ -115,41 +112,27 @@ static void sev_handler(int signo, siginfo_t *info, void *cx_)
     cx->uc_mcontext->__ss.__pc = cx->uc_mcontext->__ss.__lr;
 }
 
-/* Recovering from Non-Executable Page Access, set x0 to 0xdeadbeef (0x41414141), set pc +4, jmp to link register (__ss.__lr) to Return 
-      See URL https://github.com/apple/darwin-xnu/blob/main/bsd/sys/_types/_ucontext.h for struct def
-*/
 static void bus_handler(int signo, siginfo_t *info, void *cx_)
 {
+/*    printf("Now in bus_handler\n"); */
     (void)signo;
     (void)info;
     ucontext_t *cx = cx_;
-
-/* magic pattern like 0x41414141 or 0xdeadbeef */
     cx->uc_mcontext->__ss.__x[0] = 0xdeadbeef;
-	
-/*  
- The Program Counter (PC) is accessed as PC (or R15). It is incremented by the size of the instruction executed 
- (which is always four bytes (2 instructions ahead) in ARM state). Branch instructions load the destination address into PC.   
- TODO: Fuzz pc .. note the +32 gives S3_6_c15_c1 Bit 20 rwx
- set pc +4, jmp to link register (__ss.__lr) to Return
-*/
     cx->uc_mcontext->__ss.__pc += 4;
 }
 
-/* move status register operation for S3_6_c15_c1_5 */
 static void write_sprr_perm(uint64_t v)
 {
+    printf("Now in write_sprr_perm\n");
     __asm__ __volatile__("msr S3_6_c15_c1_5, %0\n"
                          "isb sy\n" ::"r"(v)
                          :);
 }
 
-/* 
-   Move System Register into a general-purpose register... 
-   Check the Permissions 
-*/
 static uint64_t read_sprr_perm(void)
 {
+    printf("Now in read_sprr_perm\n");
     uint64_t v;
     __asm__ __volatile__("isb sy\n"
                          "mrs %0, S3_6_c15_c1_5\n"
@@ -157,10 +140,11 @@ static uint64_t read_sprr_perm(void)
     return v;
 }
 
-/* Load the Offset, Can we Read */
 static bool can_read(void *ptr)
 {
+    printf("Now in can_read\n");
     uint64_t v = 0;
+
     __asm__ __volatile__("ldr x0, [%0]\n"
                          "mov %0, x0\n"
                          : "=r"(v)
@@ -172,16 +156,15 @@ static bool can_read(void *ptr)
     return true;
 }
 
-/* Store the Offset, Can we Write */
 static bool can_write(void *ptr)
 {
+    printf("Now in can_write\n");
     uint64_t v = 0;
+
     __asm__ __volatile__("str x0, [%0]\n"
                          "mov %0, x0\n"
                          : "=r"(v)
-			 : "r"(ptr + 8)
-/* TODO Twiddle the ptr + x for Fuzzing
-/*  			 : "r"(ptr + 32) */
+                         : "r"(ptr + 4)
                          : "memory", "x0");
 
     if (v == 0xdeadbeef)
@@ -189,9 +172,9 @@ static bool can_write(void *ptr)
     return true;
 }
 
-/* Can we Execute */
 static bool can_exec(void *ptr)
 {
+    printf("Now in can exec\n");
     uint64_t (*fun_ptr)(uint64_t) = ptr;
     uint64_t res = fun_ptr(0);
     if (res == 0xdeadbeef)
@@ -199,83 +182,85 @@ static bool can_exec(void *ptr)
     return true;
 }
 
-/* What were the results */
 static void sprr_test(void *ptr, uint64_t v)
 {
+    printf("Now in sprr_test\n");
     uint64_t a, b;
     a = read_sprr_perm();
+    printf("after read_sprr_perm v:%llx\n", a);
     write_sprr_perm(v);
+    printf("after write_sprr_perm v:%llx\n", v);
     b = read_sprr_perm();
-/* Print Results */
-    printf("%llx: %c%c%c\n", b, can_read(ptr) ? 'r' : '-', can_write(ptr) ? 'w' : '-',
+    printf("after write_sprr_perm v:%llx\n", b);
+    
+    printf("Final Value:%llx: %c%c%c\n", b, can_read(ptr) ? 'r' : '-', can_write(ptr) ? 'w' : '-',
            can_exec(ptr) ? 'x' : '-');
+/*        printf("----begin added printfs-----\n\r"); 
+        printf("ptr: %u\n", ptr);
+        printf("a:%llx\n", a);
+        printf("v:%llx\n", v);
+        printf("memory:%llu\n", "memory");
+        printf("bv %02d: %016llx\n", b, v);
+        printf("va:%c%c%c\n %u\n", v, a);
+        printf("b:%c\n", b);
+        printf("a:%llx\n", a);
+        printf("----end added printfs-----\n\r");
+*/
+/*    printf("bit %02d: %016llx\n", i, read_sprr()); */
 }
 
-/* Make it rwx */
 static uint64_t make_sprr_val(uint8_t nibble)
 {
     uint64_t res = 0;
     for (int i = 0; i < 16; ++i)
-
-/* Nibble 4 * i bytes at a time.. this is where undefined behavior creeps in ...  
-needs to be 16-byte aligned at all the time to not be undefined by C spec */
         res |= ((uint64_t)nibble) << (4 * i);
     return res;
 }
 
-/* MAIN FUNCTION */
+uint64_t read_sprr(void)
+{
+    uint64_t v;
+    __asm__ __volatile__("isb sy\n"
+                         "mrs %0, S3_6_c15_c1_5\n"
+                         : "=r"(v)::"memory");
+    return v;
+}
+
 int main(int argc, char *argv[])
 {
+    printf("Start inside Main\n");
     (void)argc;
     (void)argv;
-
+    printf("Now hitting struct sigaction\n");
     struct sigaction sa;
+    printf("Now hitting sigfillset(&sa.sa_mask)\n");
     sigfillset(&sa.sa_mask);
+    printf("Now hitting sa.sa_sigaction = bus_handler\n");
     sa.sa_sigaction = bus_handler;
+    printf("Now hitting sa.sa_flags = SA_RESTART | SA_SIGINFO\n");
     sa.sa_flags = SA_RESTART | SA_SIGINFO;
+    printf("Now hitting sigaction(SIGBUS, &sa, 0)\n");
     sigaction(SIGBUS, &sa, 0);
+    printf("Now hitting sa.sa_sigaction = sev_handler\n");
     sa.sa_sigaction = sev_handler;
+    printf("Now hitting sigaction(SIGSEGV, &sa\n");
     sigaction(SIGSEGV, &sa, 0);
-
-/* map a page with MAP_JIT and try to read, write or execute that memory for all four possible values in the system register */
-
+    printf("Now hitting uint32_t *ptr = mmap(NULL, 0x4000, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS | MAP_JIT, -1, 0)\n");
     uint32_t *ptr = mmap(NULL, 0x4000, PROT_READ | PROT_WRITE | PROT_EXEC,
                          MAP_PRIVATE | MAP_ANONYMOUS | MAP_JIT, -1, 0);
-   
-		/* Flip some bits - 64-bit */
-		   
     write_sprr_perm(0x3333333333333333);
-    
-    /* 
-A64 opcode reference: https://developer.arm.com/docs/ddi0487/latest
-Use an enum here rather than separate constexpr vars because otherwise some
-of the vars will end up unused on each platform, upsetting
--Wunused-const-variable.
-
-enum : Instruction {
- There are multiple valid encodings of return (which is really a special
-  form of branch). This is the one clang seems to use:
-  
-  kRet = 0xd65f03c0, Works fine on Big Sur .. 
-  kBrk0 = 0xd4200000, Doesn't work on Big Sur .. Trace/BPT trap: 5
-  kBrk1 = 0xd4200020, Doesn't work on Big Sur .. Trace/BPT trap: 5
-  kBrkF000 = 0xd43e0000, Doesn't work on Big Sur .. Trace/BPT trap: 5
-  kHlt0 = 0xd4400000, Doesn't work on Big Sur .. Illegal instruction: 4
-  0xD65F0FFF = Did get a valid program execution 
-  eg:
- ./0xD65F0FFF
-2010000030000000: ---
-2010000030100000: r--
-2010000030200000: r--
-2010000030300000: rw-
-  
-  */
-    
+    printf("Now in main at RET ptr[0] 0xd65f03c0\n");
     ptr[0] = 0xd65f03c0; // ret
-
+    printf("Hitting for (int i = 0; i < 4; ++i)\n");
     for (int i = 0; i < 4; ++i)
         sprr_test(ptr, make_sprr_val(i));
+    
+    printf("Bottom of main{}");
+/*        return 0;
+ */
 }
+
+
 
 /* 
 
