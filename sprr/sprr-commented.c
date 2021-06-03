6a50d93cc3ea3a15553ce9db9cb6ca620ac0e8a7
Hoyt LLC Research <xss@xss.cx>
2021-06-03 11:24:24 -0400

    Update Code Comments
    Update Code comments

--- sprr/sprr-commented.c
+++ sprr/sprr-commented.c
@@ -126,25 +126,27 @@ static void bus_handler(int signo, siginfo_t *info, void *cx_)
 
 static void write_sprr_perm(uint64_t v)
 {
-    printf("Now in write_sprr_perm\n");
+    printf("Start write_sprr_perm\n");
     __asm__ __volatile__("msr S3_6_c15_c1_5, %0\n"
                          "isb sy\n" ::"r"(v)
                          :);
+    printf("End write_sprr_perm\n");
 }
 
 static uint64_t read_sprr_perm(void)
 {
-    printf("Now in read_sprr_perm\n");
+    printf("Start read_sprr_perm\n");
     uint64_t v;
     __asm__ __volatile__("isb sy\n"
                          "mrs %0, S3_6_c15_c1_5\n"
                          : "=r"(v)::"memory");
     return v;
+    printf("End read_sprr_perm\n");
 }
 
 static bool can_read(void *ptr)
 {
-    printf("Now in can_read\n");
+    printf("Start can_read\n");
     uint64_t v = 0;
 
     __asm__ __volatile__("ldr x0, [%0]\n"
@@ -156,11 +158,12 @@ static bool can_read(void *ptr)
     if (v == 0xdeadbeef)
         return false;
     return true;
+    printf("End can_read\n");
 }
 
 static bool can_write(void *ptr)
 {
-    printf("Now in can_write\n");
+    printf("Start can_write\n");
     uint64_t v = 0;
 
     __asm__ __volatile__("str x0, [%0]\n"
@@ -172,16 +175,18 @@ static bool can_write(void *ptr)
     if (v == 0xdeadbeef)
         return false;
     return true;
+    printf("End can_write\n");
 }
 
 static bool can_exec(void *ptr)
 {
-    printf("Now in can exec\n");
+    printf("Start can_exec\n");
     uint64_t (*fun_ptr)(uint64_t) = ptr;
     uint64_t res = fun_ptr(0);
     if (res == 0xdeadbeef)
         return false;
     return true;
+    printf("End can_exec\n");
 }
 
 static void sprr_test(void *ptr, uint64_t v)
@@ -191,29 +196,47 @@ static void sprr_test(void *ptr, uint64_t v)
     a = read_sprr_perm();
     printf("after a = read_sprr_perm a:%llx\n", a);
     write_sprr_perm(v);
-    printf("after write_sprr_perm(v) v:%llx\n", v);
+    printf("after write_sprr_perm v:%llx\n", v);
     b = read_sprr_perm();
     printf("after b = write_sprr_perm b:%llx\n", b);
     
     printf("Final Value:%llx: %c%c%c\n", b, can_read(ptr) ? 'r' : '-', can_write(ptr) ? 'w' : '-',
            can_exec(ptr) ? 'x' : '-');
+    printf("End of  sprr_test..........\n");
+
+/*        printf("----begin added printfs-----\n\r"); 
+        printf("ptr: %u\n", ptr);
+        printf("a:%llx\n", a);
+        printf("v:%llx\n", v);
+        printf("memory:%llu\n", "memory");
+        printf("bv %02d: %016llx\n", b, v);
+        printf("va:%c%c%c\n %u\n", v, a);
+        printf("b:%c\n", b);
+        printf("a:%llx\n", a);
+        printf("----end added printfs-----\n\r");
+*/
+/*    printf("bit %02d: %016llx\n", i, read_sprr()); */
 }
 
 static uint64_t make_sprr_val(uint8_t nibble)
 {
+    printf("Start make_sprr_test\n");
     uint64_t res = 0;
     for (int i = 0; i < 16; ++i)
         res |= ((uint64_t)nibble) << (4 * i);
     return res;
+    printf("End make_sprr_test\n");
 }
 
 uint64_t read_sprr(void)
 {
+    printf("Start read_sprr\n");
     uint64_t v;
     __asm__ __volatile__("isb sy\n"
                          "mrs %0, S3_6_c15_c1_5\n"
                          : "=r"(v)::"memory");
     return v;
+    printf("End read_sprr\n");
 }
 
 int main(int argc, char *argv[])
@@ -240,27 +263,15 @@ int main(int argc, char *argv[])
                          MAP_PRIVATE | MAP_ANONYMOUS | MAP_JIT, -1, 0);
     write_sprr_perm(0x3333333333333333);
     printf("Now in main at RET ptr[0] 0xd65f03c0\n");
-/*
- There are multiple valid encodings of return (which is really a special
-  form of branch). This is the one clang seems to use:
-   kRet = 0xd65f03c0, Works fine on Big Sur .. 
-  kBrk0 = 0xd4200000, Doesn't work on Big Sur .. Trace/BPT trap: 5
-  kBrk1 = 0xd4200020, Doesn't work on Big Sur .. Trace/BPT trap: 5
-  kBrkF000 = 0xd43e0000, Doesn't work on Big Sur .. Trace/BPT trap: 5
-  kHlt0 = 0xd4400000, Doesn't work on Big Sur .. Illegal instruction: 4  
-  0xD65F0FFF = Did get a valid program execution 
-*/
     ptr[0] = 0xd65f03c0; // ret
+    printf("ptr %p\n", ptr);
     printf("Hitting for (int i = 0; i < 4; ++i)\n");
     for (int i = 0; i < 4; ++i)
         sprr_test(ptr, make_sprr_val(i));
-    
-    printf("Bottom of main{}");
+    printf("Bottom of main{}\n");
 /*        return 0;
  */
 }
-
-
 
 /* 
 
