7353e72d66208dd0b31506a9649e24047544247e
David Hoyt <h02332@gmail.com>
2023-08-15 10:09:34 -0400

    Update pilgrimage.cpp

--- code/pilgrimage/pilgrimage.cpp
+++ code/pilgrimage/pilgrimage.cpp
@@ -3,7 +3,7 @@
  Compile:
  clang++  pilgrimage.cpp -lpng -lz -o pilgrimage -I/opt/homebrew/include -L/opt/homebrew/lib
  Run:
- ./pilgrimage foobar
+ ./pilgrimage /etc/passwd
  Based on the pilgrimage bug: The researchers at MetabaseQ discovered CVE-2022-44268, i.e. ImageMagick 7.1.0-49 is vulnerable to Information Disclosure. When it parses a PNG image (e.g., for resize), the resulting image could have embedded the content of an arbitrary remote file (if the ImageMagick binary has permissions to read it).
  */
 #include <iostream>
