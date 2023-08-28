5f67b02afa280c3b6a261a554771a58fba9a6bf1
David Hoyt <h02332@gmail.com>
2023-08-28 18:31:46 -0400

    Update build-ballistic-fuzz-test.zsh

--- code/png/build-ballistic-fuzz-test.zsh
+++ code/png/build-ballistic-fuzz-test.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Compile the program with profiling enabled
-clang++  -fprofile-instr-generate -fcoverage-mapping -mllvm -runtime-counter-relocation -g -fsanitize=undefined -O0 -o ball-fuzz19 ball-fuzz19.cpp -std=c++11 `Magick++-config --cppflags --cxxflags --ldflags --libs`
+clang++  -fprofile-instr-generate -fcoverage-mapping -mllvm -runtime-counter-relocation -g -fsanitize=undefined -O0 -o ballistic-fuzz-sample-001.cpp ballistic-fuzz-sample-001 -std=c++11 `Magick++-config --cppflags --cxxflags --ldflags --libs`
 
 # Check if compilation was successful
 if [[ $? != 0 ]]; then
@@ -10,19 +10,19 @@ if [[ $? != 0 ]]; then
 fi
 
 # Run the program to generate the profiling data
-LLVM_PROFILE_FILE=default.profraw ./ball-fuzz19
+LLVM_PROFILE_FILE=default.profraw ./ballistic-fuzz-sample-001
 
 # Merge the profiling data
 xcrun llvm-profdata merge -sparse default.profraw -o a.profdata
 
 # Generate the textual report
-xcrun llvm-cov report ./ball-fuzz19 -instr-profile=a.profdata
+xcrun llvm-cov report ./ballistic-fuzz-sample-001 -instr-profile=a.profdata
 
 # Generate the HTML report and save it to a directory
-xcrun llvm-cov show ./ball-fuzz19 -instr-profile=a.profdata -format=html -output-dir=./coverage_report
+xcrun llvm-cov show ./ballistic-fuzz-sample-001 -instr-profile=a.profdata -format=html -output-dir=./coverage_report
 
 # Alternatively, if you prefer a single HTML file, uncomment the line below:
-xcrun llvm-cov show ./ball-fuzz19 -instr-profile=a.profdata --format=html > coverage.html
+xcrun llvm-cov show ./ballistic-fuzz-sample-001 -instr-profile=a.profdata --format=html > coverage.html
 
 echo "Done!"
 
