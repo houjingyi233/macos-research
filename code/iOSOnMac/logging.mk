2d6b40918a0543ae1d0b22e60873ae7f440626dd
David Hoyt <h02332@gmail.com>
2023-11-26 08:39:22 -0500

    Update logging.mk
    Cleanup CopyPasta

--- code/iOSOnMac/logging.mk
+++ code/iOSOnMac/logging.mk
@@ -1,42 +1,27 @@
 export VERBOSE ?= 0
-# If VERBOSE is set to 0, we will suppress echoing
-ifeq (0, ${VERBOSE})
-# Suppress echoing
+
+# Control echoing based on VERBOSE
+ifeq (0, $(VERBOSE))
 .SILENT:
 endif
 
-# If VERBOSE is 0 we will redirect to /dev/null
-export _REDIRECT_OUTPUT=$(if $(ifeq (0, ${VERBOSE})), " > /dev/null",)
-export EXTRA_MAKE_FLAGS=$(if $(ifeq (0, ${VERBOSE})), -s,)
+# Redirect output if VERBOSE is 0
+export _REDIRECT_OUTPUT = $(if $(findstring 0, $(VERBOSE)), > /dev/null)
+export EXTRA_MAKE_FLAGS = $(if $(findstring 0, $(VERBOSE)), -s)
 
+# Define color codes for logging
 export BLUE := tput -Txterm setaf 6
 export GREEN := tput -Txterm setaf 2
 export WHITE := tput -Txterm setaf 7
 export RED := tput -Txterm setaf 1
 export RESET := tput -Txterm sgr0
 
+# Set project display name
 export PROJECT_DISPLAY_NAME := $(or ${PROJECT_NAME}, $(notdir $(shell pwd)))
+
+# Define logging functions
 log = @echo [$(PROJECT_DISPLAY_NAME)] - $(1)
-
-define log_build
-${BLUE}
-$(call log, [++] Building $(notdir $(shell pwd)))
-${RESET}
-endef
-
-define log_clean
-${WHITE}
-$(call log, [---] Cleaning $(notdir $(shell pwd)))
-${RESET}
-endef
-
-define log_die
-${RED}
-@echo $1
-${RESET}
-@exit 1
-endef
-
-define check_path
-which $1 >/dev/null || ($(RED) && echo "[$(PROJECT_DISPLAY_NAME)] - $2" && $(RESET) && exit 1)
-endef
+log_build = ${BLUE} && $(call log, [++] Building $(notdir $(shell pwd))) && ${RESET}
+log_clean = ${WHITE} && $(call log, [---] Cleaning $(notdir $(shell pwd))) && ${RESET}
+log_die = ${RED} && @echo $1 && ${RESET} && @exit 1
+check_path = which $1 >/dev/null || ($(RED) && echo "[$(PROJECT_DISPLAY_NAME)] - $2" && $(RESET) && exit 1)
